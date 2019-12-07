3edae7cfbedc2563b8b3a89d4c9aba2284c27899
arwensadler <42981061+arwensadler@users.noreply.github.com>
2019-12-07 15:20:33 -0500

    Update led_test.ino

--- led_test/led_test.ino
+++ led_test/led_test.ino
@@ -4,7 +4,7 @@
 #define LED_COUNT 5
 Adafruit_NeoPixel strip(LED_COUNT, D_IN, NEO_GRB + NEO_KHZ800);
 
-uint32_t color = strip.Color(0, 255, 0);
+uint32_t color = strip.Color(137, 255, 0);
 int first = 0;
 int count = 5;
 
