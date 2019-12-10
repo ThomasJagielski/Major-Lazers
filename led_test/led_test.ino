27bc954a6c17dcd050b4183f37ed81d3d520396f
arwensadler <42981061+arwensadler@users.noreply.github.com>
2019-12-09 23:28:02 -0500

    Final code

--- led_test/led_test.ino
+++ led_test/led_test.ino
@@ -1,37 +1,23 @@
 #include <Adafruit_NeoPixel.h>
 
 #define D_IN 13
-#define LED_COUNT 5
+#define LED_COUNT 60
 Adafruit_NeoPixel strip(LED_COUNT, D_IN, NEO_GRB + NEO_KHZ800);
 
 uint32_t color = strip.Color(137, 255, 0);
 int first = 0;
-int count = 5;
+int count = 60;
 
 void setup() {
   // put your setup code here, to run once:
   strip.begin();
   strip.show();
-
+  strip.fill(color,0,59);
+  strip.show();
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  strip.setPixelColor(0,color);
-  delay(1000);
-  strip.show();
-  strip.setPixelColor(1,color);
-  delay(1000);
-  strip.show();
-  strip.setPixelColor(2,color);
-  delay(1000);
-  strip.show();
-  strip.setPixelColor(3,color);
-  delay(1000);
-  strip.show();
-  strip.setPixelColor(4,color);
-  delay(1000);
-  strip.show();
-  strip.clear();
+  
 }
